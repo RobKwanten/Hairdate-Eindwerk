4a10a31f82a9cc5994610cd2ba78d9874e681e2f
RobKwanten <robkwanten@hotmail.com>
2020-06-29 19:16:03 +0200

    Laatste wijzigingen

--- public/index2.php
+++ public/index2.php
@@ -1,1 +1,18 @@
 <?php
+
+use App\Kernel;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\Request;
+
+//require __DIR__.'/../vendor/autoload.php';  
+
+//for local development 
+
+//require __DIR__.'/../config/bootstrap.php';  
+
+//for deployment system 3 
+require 'system/vendor/autoload.php';
+
+// The check is to ensure we don't use .env in production 
+(new Dotenv())->load(__DIR__.'/system/.env.local');
